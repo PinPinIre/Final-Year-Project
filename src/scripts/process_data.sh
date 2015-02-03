5570e6bb480bc18b538b1736890871b443ae0c22
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-03 21:55:00 +0000

    Update process data script

--- src/scripts/process_data.sh
+++ src/scripts/process_data.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 function processFile {
-    pandoc -f latex -t plain $1 -o "${1}.txt"
+    iconv -f WINDOWS-1252 -t UTF-8 $1 > "${1}.tex"
+    pandoc -f latex -t plain "${1}.tex" -o "${1}.txt"
     detex "${1}.txt"        # Strip any remaining LaTeX from file. Data is noisey
 }
 
 export -f processFile   # Make function executable by parallel
 DIR=$1
-FILES=`find ${DIR} -type f ! -name "*.*"`
-parallel processFile ::: $FILES
+find ${DIR} -type f ! -name "*.*" | parallel processFile ::: $FILES
 
