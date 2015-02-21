f1f500488197a40a007af67c2c8dcc0aaef60dd3
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-21 23:55:42 +0000

    Add functionality to load/save w2v model

--- src/project/word2vec_corpus.py
+++ src/project/word2vec_corpus.py
@@ -6,20 +6,42 @@ from gensim import models
 
 class W2VCorpus(Corpus):
 
-    def __init__(self, dir, dict_loc, vec_loc):
+    def __init__(self, dict_loc, vec_loc, dir=None):
         Corpus.__init__(self, dir)
-        # Todo: Tweak the default paramaters
-        self.model = models.Word2Vec(self.docs.get_texts(), size=100, window=5, min_count=5, workers=4)
+        self.dict_loc = dict_loc
+        self.vec_loc = vec_loc
+        self.model = None
+        if dir:
+            # Todo: Tweak the default paramaters
+            self.model = models.Word2Vec(self.docs.get_texts(), size=100, window=5, min_count=5, workers=4)
         return
 
     def similarity(self, word1, word2):
-        return self.model.similarity(word1, word2)
+        if self.model:
+            return self.model.similarity(word1, word2)
+        else:
+            # Todo: Raise exception?
+            return None
+
+    def save(self, file):
+        super(W2VCorpus, self).save(self.dict_loc, self.vec_loc)
+        if self.model: self.model.save(file)
+
+    def load(self, file):
+        super(W2VCorpus, self).load(self.dict_loc, self.vec_loc)
+        if self.model: self.model.load(file)
 
 
 def main():
+    w2v = "w2vcorpus.w2v"
     if len(sys.argv) > 2 and isdir(sys.argv[1]) and isfile(sys.argv[2]) and isfile(sys.argv[3]):
-        corpus = W2VCorpus(sys.argv[1], sys.argv[2], sys.argv[3])
-        #print "Sim: column <-> row\t" + str(corpus.similarity("column", "row"))
+        if not isfile(w2v):
+            corpus = W2VCorpus(sys.argv[2], sys.argv[3], sys.argv[1])
+            corpus.save(w2v)
+        else:
+            corpus = W2VCorpus(sys.argv[2], sys.argv[3])
+            corpus.load(w2v)
+        print "Sim: velocity <-> speed\t" + str(corpus.similarity("velocity", "speed"))
     else:
         print "Corpus requires directory as an argument."
 
