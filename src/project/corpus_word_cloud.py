f8ba53434e7ac1867d9f3a5b72c1bf992676832b
Cathal Geoghegan <pinpinire@gmail.com>
2015-02-25 17:12:30 +0000

    Add functionality to generate word clouds

--- src/project/corpus_word_cloud.py
+++ src/project/corpus_word_cloud.py
@@ -15,12 +15,12 @@ class CorpusWordCloud(object):
 
     def get_topics(self, num_words=20):
         topics = self.corpus.print_topics(num_words=num_words)
-        return map(WordCloud.parse_topics, topics)
+        return map(CorpusWordCloud.parse_topics, topics)
 
-    def draw_topics(self, no_topics=10, num_words=20):
+    def draw_topics(self, no_topics=10, num_words=30):
         topics = self.get_topics(num_words=num_words)
         for id, topic in enumerate(topics):
-            WordCloud.draw_topic(topic, id)
+            CorpusWordCloud.draw_topic(topic, id)
 
     @staticmethod
     def parse_topics(string):
@@ -29,14 +29,14 @@ class CorpusWordCloud(object):
         for word in words:
             word_freq = unicode.split(word, "*")
             if len(word_freq) is 2:
-                word_topics[word_freq[1]] = word_freq[0]
+                word_topics[word_freq[1]] = float(word_freq[0])
         return word_topics
 
     @staticmethod
     def draw_topic(topic, id):
-        wordcloud = WordCloud()
-        elements = wordcloud.fit_words(topic.items(), width=120, height=120)
-        wordcloud.draw(elements, "gs_topic_%d.png" % (id), width=120, height=120)
+        wordcloud = WordCloud(width=500, height=500, font_path="/System/Library/Fonts/Monaco.dfont", background_color="white")
+        elements = wordcloud.fit_words(topic.items())
+        wordcloud.to_file("/Users/cathalgeoghegan/Desktop/fypimages/" + str(id) + ".png")
 
     @classmethod
     def load(cls, dictionary, corpus, lda_file):
