784c331629c8915c7739ec0fa478202c3e34d1da
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-10 23:27:46 +0000

    Extend algorithm harness

--- src/run_algorithm.py
+++ src/run_algorithm.py
@@ -1,10 +1,41 @@
 import argparse
-from os.path import isdir
+import datetime
+from os import makedirs
+from os.path import isdir, exists
 from project import corpus, knn_corpus, lda_corpus, word2vec_corpus
 
 algorithms = {"lda": lda_corpus.LDACorpus,
               "knn": knn_corpus.KNNCorpus,
               "w2v": word2vec_corpus.W2VCorpus}
+
+output_loc = "/corpus_out"
+dictionary_loc = output_loc + "/corpus.dict"
+corpus_loc = output_loc + "/%dcorpus.mm"
+log_file = output_loc + "/runtimes.log"
+
+
+def run_algo(directory, ints, algorithm):
+    # Build corpus from largest int and directory (Check valid directory)
+    if isdir(directory):
+        max_corpus = max(ints)
+        if not exists(output_loc):
+            makedirs(output_loc)
+        log = open(log_file, 'a+')
+        base_corpus_file = corpus_loc % max_corpus
+        start_time = datetime.datetime.now()
+        # Build corpus of size max_corpus
+        base_corpus = corpus.Corpus(directory=directory)
+        base_corpus.save(dictionary_loc, base_corpus_file)
+        end_time = datetime.datetime.now()
+        base_corpus_build_time = end_time - start_time
+        log.write(("base_corpus_build_time %d:\t" % max_corpus) + str(base_corpus_build_time))
+        for size in ints:
+            # For each int in the param list then apply the corpus algorithm using a sliced corpus
+            # Save any mm, index files, etc to a directory so they can be used again.
+            test_corpus = algorithms[algorithm](dictionary=dictionary_loc, corpus=corpus_loc, max_docs=size)
+            # Log temporal time)
+            log.write(("%s %d train time:\t" % (algorithm, size)) + test_corpus.get_train_time())
+
 
 def main():
     parser = argparse.ArgumentParser(description='Build bow corpus on the arxiv corpus.')
@@ -12,15 +43,7 @@ def main():
     parser.add_argument('directory', help='directory for the arxiv txt files')
     parser.add_argument('algorithm', help='algoritm to apply to the corpus', choices=algorithms)
     args = parser.parse_args()
-    # Build corpus from largest int and directory (Check valid directory)
-    if isdir(args.directory):
-        max_corpus = max(args.integers)
-        # Build corpus of size max_corpus
-        for size in args.integers:
-            print size
-            # For each int in the param list then apply the corpus algorithm using a sliced corpus
-            # Save any mm, index files, etc to a directory so they can be used again.
-            # Log temporal time
+    run_algo(args.directory, args.integers, args.algorithm)
     else:
         print "Directory argument should be a valid directory"
 
