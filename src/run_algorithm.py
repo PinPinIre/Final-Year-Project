c628aae61b6643fcfcee6ae4a592d0169de4fca6
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-10 23:39:57 +0000

    Fix silly copy and paste

--- src/run_algorithm.py
+++ src/run_algorithm.py
@@ -16,26 +16,25 @@ log_file = output_loc + "/runtimes.log"
 
 def run_algo(directory, ints, algorithm):
     # Build corpus from largest int and directory (Check valid directory)
-    if isdir(directory):
-        max_corpus = max(ints)
-        if not exists(output_loc):
-            makedirs(output_loc)
-        log = open(log_file, 'a+')
-        base_corpus_file = corpus_loc % max_corpus
-        start_time = datetime.datetime.now()
-        # Build corpus of size max_corpus
-        base_corpus = corpus.Corpus(directory=directory)
-        base_corpus.save(dictionary_loc, base_corpus_file)
-        end_time = datetime.datetime.now()
-        base_corpus_build_time = end_time - start_time
-        log.write(("base_corpus_build_time %d:\t" % max_corpus) + str(base_corpus_build_time))
-        for size in ints:
-            # For each int in the param list then apply the corpus algorithm using a sliced corpus
-            test_corpus = algorithms[algorithm](dictionary=dictionary_loc, corpus=base_corpus_file, max_docs=size)
-            # Save any mm, index files, etc to a directory so they can be used again.
-            # test_corpus.save() TODO: Modify save so it is general accross all algorithms.
-            # Log temporal time
-            log.write(("%s %d train time:\t" % (algorithm, size)) + test_corpus.get_train_time())
+    max_corpus = max(ints)
+    if not exists(output_loc):
+        makedirs(output_loc)
+    log = open(log_file, 'a+')
+    base_corpus_file = corpus_loc % max_corpus
+    start_time = datetime.datetime.now()
+    # Build corpus of size max_corpus
+    base_corpus = corpus.Corpus(directory=directory)
+    base_corpus.save(dictionary_loc, base_corpus_file)
+    end_time = datetime.datetime.now()
+    base_corpus_build_time = end_time - start_time
+    log.write(("base_corpus_build_time %d:\t" % max_corpus) + str(base_corpus_build_time))
+    for size in ints:
+        # For each int in the param list then apply the corpus algorithm using a sliced corpus
+        test_corpus = algorithms[algorithm](dictionary=dictionary_loc, corpus=base_corpus_file, max_docs=size)
+        # Save any mm, index files, etc to a directory so they can be used again.
+        # test_corpus.save() TODO: Modify save so it is general accross all algorithms.
+        # Log temporal time
+        log.write(("%s %d train time:\t" % (algorithm, size)) + test_corpus.get_train_time())
 
 
 def main():
@@ -44,7 +43,8 @@ def main():
     parser.add_argument('directory', help='directory for the arxiv txt files')
     parser.add_argument('algorithm', help='algoritm to apply to the corpus', choices=algorithms)
     args = parser.parse_args()
-    run_algo(args.directory, args.integers, args.algorithm)
+    if isdir(args.directory):
+        run_algo(args.directory, args.integers, args.algorithm)
     else:
         print "Directory argument should be a valid directory"
 
