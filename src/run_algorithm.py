3954aa7015fd4fe40aa90070b0da4546c65af51a
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-12 21:52:41 +0000

    Update saving of corpus

--- src/run_algorithm.py
+++ src/run_algorithm.py
@@ -9,9 +9,10 @@ algorithms = {"lda": lda_corpus.LDACorpus,
               "w2v": word2vec_corpus.W2VCorpus}
 
 output_loc = "/corpus_out"
-dictionary_loc = output_loc + "/corpus.dict"
+dictionary_loc = output_loc + "/%dcorpus.dict"
 corpus_loc = output_loc + "/%dcorpus.mm"
 log_file = output_loc + "/runtimes.log"
+sup_file_loc = output_loc + "/%d.%s"
 
 
 def run_algo(directory, ints, algorithm):
@@ -22,19 +23,28 @@ def run_algo(directory, ints, algorithm):
     log = open(log_file, 'a+')
     base_corpus_file = corpus_loc % max_corpus
     start_time = datetime.datetime.now()
+
     # Build corpus of size max_corpus and save to be reused
     base_corpus = corpus.Corpus(directory=directory, max_docs=max_corpus)
-    base_corpus.save(dictionary_loc, base_corpus_file)
+    base_corpus.save(dictionary_loc % max_corpus, base_corpus_file)
     end_time = datetime.datetime.now()
     base_corpus_build_time = end_time - start_time
     log.write(("base_corpus_build_time %d:\t" % max_corpus) + str(base_corpus_build_time))
+
+    # For each int in the param list then apply the corpus algorithm using a sliced corpus
     for size in ints:
-        # For each int in the param list then apply the corpus algorithm using a sliced corpus
+        if size is max_corpus: continue
         test_corpus = algorithms[algorithm](dictionary=dictionary_loc, corpus=base_corpus_file, max_docs=size)
+
         # Save any mm, index files, etc to a directory so they can be used again.
-        # test_corpus.save() TODO: Modify save so it is general accross all algorithms.
+        dict_loc = dictionary_loc % size
+        corp_loc = corpus_loc % size
+        sup_loc = sup_file_loc % (size, algorithm)
+        test_corpus.save(dictionary_file=dict_loc, corpus_file=corp_loc, sup_file=sup_loc)  # TODO: Modify save so it is general accross all algorithms.
+
         # Log temporal time
         log.write(("%s %d train time:\t" % (algorithm, size)) + test_corpus.get_train_time())
+    log.close()
 
 
 def main():
