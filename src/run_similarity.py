fe49796099f5ce21f117c0140f30083cd887b3fc
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-20 22:36:45 +0000

    customise for word2vec

--- src/run_similarity.py
+++ src/run_similarity.py
@@ -1,7 +1,7 @@
 import argparse
 import datetime
 from os import getcwd
-from os.path import isdir, exists
+from os.path import exists
 from project import corpus, knn_corpus, lda_corpus, word2vec_corpus
 
 algorithms = {"lda": lda_corpus.LDACorpus,
@@ -19,33 +19,37 @@ query_dict = output_loc + "/query.dict"
 query_corp = output_loc + "query.mm"
 
 
-def load_queries(query_dir, dictionary):
-    query_corpus = corpus.Corpus(directory=query_dir, dictionary=dictionary)
+def load_queries(query_dir, dictionary, algorithm):
+    if algorithm != "w2v":
+        query_corpus = corpus.Corpus(directory=query_dir, dictionary=dictionary)
+    else:
+        docs = corpus.Corpus.get_docs(query_dir)
+        query_corpus = corpus.PaperCorpus(docs).get_texts()
     return query_corpus
 
 
-def run_sim(ints, algorithm, query_files):
+def run_sim(ints, algorithm, query_files, corpus_dir):
     output_dir = output_loc % algorithm
     if not exists(output_dir):
         print "Output directory for %s must exist already. Run run_algorithm.py first." % algorithm
         return
     log = open(log_file % algorithm, 'a+')
     max_dict = dictionary_loc % (algorithm, "base_")
-    queries = load_queries(query_files, max_dict)
+    queries = load_queries(query_files, max_dict, algorithm)
     for size in ints:
         corpus_dict = dictionary_loc % (algorithm, size)
         corp = corpus_loc % (algorithm, size)
         sup_file = sup_file_loc % (algorithm, size, algorithm)
-        test_corpus = algorithms[algorithm].load(dictionary_file=corpus_dict, corpus_file=corp, sup_file=sup_file)
+        if algorithm != "w2v":
+            test_corpus = algorithms[algorithm].load(dictionary_file=corpus_dict, corpus_file=corp, sup_file=sup_file)
+        else:
+            test_corpus = algorithms[algorithm](directory=corpus_dir, max_docs=size)
 
         start_time = datetime.datetime.now()
-        if algorithm != "w2v":
-            for i, query in enumerate(queries):
-                most_sim = test_corpus.run_query(query, index_loc % (algorithm, size, algorithm), 10)
-                print most_sim
-                # TODO: Log the similarities to a file for inspection
-        else:
-            print "w2v currently not supported"
+        for i, query in enumerate(queries):
+            most_sim = test_corpus.run_query(query, index_loc % (algorithm, size, algorithm), 10)
+            print most_sim
+            # TODO: Log the similarities to a file for inspection
         end_time = datetime.datetime.now()
 
         query_time = end_time - start_time
@@ -58,8 +62,9 @@ def main():
     parser.add_argument('integers', metavar='N', type=int, nargs='+', help='size values for the corpus')
     parser.add_argument('directory', help='directory for the query files')
     parser.add_argument('algorithm', help='algorithm to apply to the corpus', choices=algorithms)
+    parser.add_argument('corp_directory', help='directory for the arxiv corpus files')
     args = parser.parse_args()
-    run_sim(args.integers, args.algorithm, args.directory)
+    run_sim(args.integers, args.algorithm, args.directory, args.corp_directory)
 
 
 if __name__ == "__main__":
