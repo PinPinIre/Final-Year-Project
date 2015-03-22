9bbbbee54ea28c27a316fd040a67916748a1d4b2
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-22 18:31:37 +0000

    Fix small file naming error

--- src/scripts/sort_corpus.sh
+++ src/scripts/sort_corpus.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 DIR=$1
-log="${DIR}paperstats.log"
+log="${DIR}/paperstats.log"
 
 NAMES="$(find ${DIR} -name '[a-z\-]*[0-9]*.*.txt' -maxdepth 1 -exec basename {} \; | grep --only-matching '^[a-z\-]*' | sort -u)"
 
 for NAME in $NAMES
 do
-    OUT_DIR="${DIR}${NAME}"
+    OUT_DIR="${DIR}/${NAME}"
 	mkdir -p $OUT_DIR
 	find ${DIR} -name "${NAME}[0-9]*.*.txt" -exec mv {} $OUT_DIR \;
 	count="$(find ${OUT_DIR} -name '*.txt' -maxdepth 1 | wc -l)"
