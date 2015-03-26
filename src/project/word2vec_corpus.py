b9c214d8bdd94388eac5ed475fb501e7b93ef63a
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-26 02:12:45 +0000

    Refactor w2v class

--- src/project/word2vec_corpus.py
+++ src/project/word2vec_corpus.py
@@ -5,38 +5,40 @@ from corpus import Corpus, PaperCorpus
 from gensim import models
 
 
+# TODO: Investigate the similarity query
 class W2VCorpus(object):
 
     def __init__(self, directory=None, w2v_model=None, max_docs=None, **kwargs):
-        docs = Corpus.get_docs(directory)
-        if max_docs: docs = docs[:max_docs]
+        docs = Corpus.get_docs(directory, distributions, max_docs)
         self.docs = PaperCorpus(docs)
         start_time = datetime.datetime.now()
         if not w2v_model:
             # Todo: Tweak the default parameter
-            self.model = models.Doc2Vec(self._gen_docs(), size=100, window=5, min_count=5, workers=4)
+            self.model = models.Doc2Vec(self._gen_docs(self.docs), size=100, window=5, min_count=5, workers=4)
         else:
             self.model = models.Doc2Vec.load(w2v_model)
         end_time = datetime.datetime.now()
         self.train_time = end_time - start_time
         return
 
-    def similarity(self, doc):
+    def similarity(self, word):
         if self.model:
-            return self.model.most_similar(doc)
+            return self.model.most_similar(word)
         else:
             # Todo: Raise exception?
             return None
 
     def save(self, dictionary_file="w2v_corpus.dict", corpus_file="corpus.mm", sup_file="vector.w2vs"):
-        if self.model: self.model.save(sup_file)
+        if self.model:
+            self.model.save(sup_file)
 
     @classmethod
     def load(cls, dictionary_file=None, corpus_file=None, sup_file=None):
         return cls(dictionary=dictionary_file, corpus=corpus_file, w2v_model=sup_file)
 
-    def _gen_docs(self,):
-        docs = self.docs.get_texts()
+    @staticmethod
+    def _gen_docs(docs):
+        docs = docs.get_texts()
         for doc_id, doc in enumerate(docs):
             yield models.doc2vec.LabeledSentence(words=doc, labels=['DOC_%s' % doc_id])
 
@@ -44,13 +46,12 @@ class W2VCorpus(object):
         return self.similarity(query)
 
 
-
 def main():
     w2v = "w2vcorpus.w2v"
     if len(sys.argv) is 4 and isdir(sys.argv[1]):
         if not isfile(w2v):
             corpus = W2VCorpus(directory=sys.argv[1])
-            corpus.save(dictionary_file=sys.argv[2], corpus_file=sys.argv[3] ,sup_file=w2v)
+            corpus.save(dictionary_file=sys.argv[2], corpus_file=sys.argv[3], sup_file=w2v)
         else:
             corpus = W2VCorpus.load(dictionary_file=sys.argv[2], corpus_file=sys.argv[3], sup_file=w2v)
         time = corpus.get_train_time()
@@ -58,4 +59,5 @@ def main():
     else:
         print "Corpus requires directory as an argument."
 
-if __name__ == "__main__": main()
+if __name__ == "__main__":
+    main()
