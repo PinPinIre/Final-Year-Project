0217350b71076b8d04a872dc235b588519cd4d8d
Cathal Geoghegan <pinpinire@gmail.com>
2015-03-30 22:25:17 +0100

    Update sorting script

--- src/scripts/sort_corpus.sh
+++ src/scripts/sort_corpus.sh
@@ -9,6 +9,13 @@ do
     OUT_DIR="${DIR}/${NAME}"
 	mkdir -p $OUT_DIR
 	find ${DIR} -name "${NAME}[0-9]*.*.txt" -exec mv {} $OUT_DIR \;
-	count="$(find ${OUT_DIR} -name '*.txt' -maxdepth 1 | wc -l)"
-	echo "${NAME}\t${count}" >> $log
+done
+
+NAMES="$(find ${DIR} -mindepth 1 -type d -maxdepth 1 -exec basename {} \; | sort -u)"
+
+for NAME in $NAMES
+do
+    OUT_DIR="${DIR}/${NAME}"
+    count="$(find ${OUT_DIR} -name '*.txt' -maxdepth 1 | wc -l)"
+    echo "${NAME}\t${count}" >> $log
 done
