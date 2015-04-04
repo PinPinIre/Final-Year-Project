818a55332b2ca604675affb54b10ca885c7b8e7c
Cathal Geoghegan <pinpinire@gmail.com>
2015-04-04 01:32:31 +0100

    log similarities to json file

--- src/run_similarity.py
+++ src/run_similarity.py
@@ -1,5 +1,6 @@
 import argparse
 import datetime
+import json
 from os import listdir
 from os.path import exists, join
 from project import corpus, knn_corpus, lda_corpus, word2vec_corpus
@@ -17,6 +18,7 @@ log_file = "sim_runtimes.log"
 index_loc = "%d%s.simindex"
 query_dict = "query.dict"
 query_corp = "query.mm"
+results_log = "query_results.json"
 
 
 def load_queries(query_dir, corp_dict, algorithm):
@@ -28,20 +30,28 @@ def load_queries(query_dir, corp_dict, algorithm):
     corp_dict = Dictionary.load(corp_dict)
     valid_files = [join(query_dir, file) for file in listdir(query_dir) if corpus.Corpus._is_corpus_file(query_dir, file)]
     query_corpus = corpus.PaperCorpus(valid_files)
-    return_vectors = list()
     for doc in query_corpus.get_texts():
         return_vectors.append(corp_dict.doc2bow(doc))
     return return_vectors
 
 
 def parse_lda_results(result_vector, directory, size):
-    result = ""
+    result = list()
     file_list = join(directory, file_logs % size)
     with open(file_list) as file:
         files = file.readlines()
     for index, sim in result_vector:
-        result = result + ("%f\t%s\n" % (sim, files[index].strip()))
+        result.append((sim, files[index].strip()))
     return result
+
+
+def format_json(output_dict, directory):
+    return_dict = dict()
+    for size, queries in output_dict.iteritems():
+        return_dict[size] = dict()
+        for query, similarities in queries.iteritems():
+            return_dict[size][query] = parse_lda_results(similarities, directory, size)
+    return return_dict
 
 
 def run_sim(ints, algorithm, query_files, directory):
@@ -49,6 +59,7 @@ def run_sim(ints, algorithm, query_files, directory):
         print "Output directory for %s must exist already. Run run_algorithm.py first." % algorithm
         return
     log = open(join(directory, log_file), 'a+')
+    query_results = dict()
     for size in ints:
         corpus_dict = join(directory, dictionary_loc % size)
         queries = load_queries(query_files, corpus_dict, algorithm)
@@ -56,17 +67,18 @@ def run_sim(ints, algorithm, query_files, directory):
         sup_file = join(directory, sup_file_loc % (size, algorithm))
         file_log = join(directory, file_logs % size)
         test_corpus = algorithms[algorithm].load(dictionary_file=corpus_dict, corpus_file=corp, sup_file=sup_file)
-
+        similarities = dict()
         start_time = datetime.datetime.now()
         for i, query in enumerate(queries):
-            most_sim = test_corpus.run_query(query, index_loc % (size, algorithm), 10)
-            if algorithm == "lda":
-                print parse_lda_results(most_sim, directory, size)
-            # TODO: Log the similarities to a file for inspection
+            similarities[i] = test_corpus.run_query(query, index_loc % (size, algorithm), 10)
         end_time = datetime.datetime.now()
 
         query_time = end_time - start_time
         log.write("%s %d query time:\t" % (algorithm, size) + str(query_time) + "\n")
+        query_results[size] = similarities
+    if algorithm == "lda":
+        query_results = format_json(query_results, directory)
+        open(join(directory, results_log), 'a+').write(json.dumps(query_results))
     log.close()
 
 
