deb703fbb8a6cc733b84408ecc4d2b8260491b04
Cathal Geoghegan <pinpinire@gmail.com>
2015-04-11 14:57:17 +0100

    Begin refactor of word2vec

--- src/project/word2vec_corpus.py
+++ src/project/word2vec_corpus.py
@@ -12,33 +12,34 @@ class W2VCorpus(object):
         if list_files:
             with open(list_files) as f:
                 docs = [line.strip() for line in f]
-        else:
+        elif directory and distributions:
             docs = Corpus.get_docs(directory, distributions, max_docs)
-        self.docs = PaperCorpus(docs)
+
         start_time = datetime.datetime.now()
         if not w2v_model:
+
+            self.docs = PaperCorpus(docs)
             # Todo: Tweak the default parameter
-            self.model = models.Doc2Vec(self._gen_docs(self.docs), size=100, window=5, min_count=5, workers=4)
+            self.model = models.Doc2Vec(self._gen_docs(self.docs), min_count=20, workers=4)
         else:
             self.model = models.Doc2Vec.load(w2v_model)
         end_time = datetime.datetime.now()
+
         self.train_time = end_time - start_time
         return
 
-    def similarity(self, word):
-        if self.model:
-            return self.model.most_similar(word)
-        else:
-            # Todo: Raise exception?
-            return None
+    def similarity(self, word, items):
+        if not self.model:
+            self.model = models.Doc2Vec(self._gen_docs(self.docs), min_count=20, workers=4)
+        return self.model.most_similar(word, topn=items)
 
     def save(self, dictionary_file="w2v_corpus.dict", corpus_file="corpus.mm", sup_file="vector.w2vs"):
         if self.model:
             self.model.save(sup_file)
 
     @classmethod
-    def load(cls, dictionary_file=None, corpus_file=None, sup_file=None):
-        return cls(w2v_model=sup_file)
+    def load(cls, sup_file=None, list_files=None):
+        return cls(w2v_model=sup_file, list_files=list_files)
 
     @staticmethod
     def _gen_docs(docs):
